f07be7108b0f243b76c5da1675456504d33e2bcd
Tonny Weslley <70965358+tonny-weslley@users.noreply.github.com>
2025-01-15 01:58:54 +0000

    feat: create graph interface class.

---
+++ src/main/java/com/mycompany/datastructures/interfaces/Graph.java
@@ -0,0 +1,37 @@
+package com.mycompany.datastructures.implementations;
+
+public interface IVertex<T> {
+    T getValue();
+    void setValue(T value);
+}
+
+
+public interface IEdge {
+    T getValue();
+    IVertex getVertex1();
+    IVertex getVertex2();
+    boolean isDirected();
+    void setValue(T value);
+    void setVertex1(IVertex vertex1);
+    void setVertex2(IVertex vertex2);
+    void setDirected(boolean directed);
+}
+
+public interface IGraph<T> {
+    void addVertex(IVertex vertex);
+    void addEdge(IEdge edge);
+    void removeVertex(IVertex vertex);
+    void removeEdge(IEdge edge);
+    List<IVertex> getVertices();
+    List<IEdge> getEdges();
+    IVertex getVertex(T value);
+    IEdge getEdge(T value);
+    boolean hasVertex(IVertex vertex);
+    boolean hasEdge(IEdge edge);
+    boolean hasEdge(IVertex vertex1, IVertex vertex2);
+    boolean hasEdge(T value);
+    boolean hasVertex(T value);
+    boolean isEmpty();
+    int size();
+    void clear();
+}
